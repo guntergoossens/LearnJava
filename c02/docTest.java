9999c093d8856847afadef0a19147aac1abfaa6a
Gunter Goossens <gunter.goossens@telenet.be>
2012-07-17 07:02:49 +0200

    up unto part 11 of chapter 2

---
+++ c02/docTest.java
@@ -0,0 +1,13 @@
+/** A class comment 
+* Let's try this <b>list</b> thingy
+* <ol>
+* <li> It's Fun
+* <li> It's Free
+* <li> It's Easy
+* </ol>
+*/
+public class docTest {
+	/** A variable comment */
+	public int i;
+	/** A method comment */
+}
